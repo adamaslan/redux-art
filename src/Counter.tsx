d468e92952639381c8c9779d02c951f42d14a244
Adam <chillcoders@gmail.com>
2021-10-06 22:50:32 -0400

    slice1

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 
-import { useAppSelector, useAppDispatch } from "app/hooks";
+import { useAppSelector, useAppDispatch } from "./hooks";
 
 import { decrement, increment } from "./counterSlice";
 
 export function Counter() {
   // The `state` arg is correctly typed as `RootState` already
-  const count = useAppSelector((state) => state.counter.value);
+  const count = useAppSelector((state) => state.counterSlice.value);
   const dispatch = useAppDispatch();
-
+  console.log(count);
+  return (
+    <div>
+      {" "}
+      <button onClick={() => dispatch(increment())}>INCREMENT </button>
+    </div>
+  );
   // omit rendering logic
 }
